70924c8d4d4990e764ff5deb3855eb0fd2bdc2db
Saumya92 <Saumya92@users.noreply.github.com>
2025-01-21 18:53:28 +0530

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import TravelForm from '../app/components/travel-form'
+import '../app/globals.css'
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-[#E8C4B8]">
+      <TravelForm />
+    </main>
+  )
+}
+
